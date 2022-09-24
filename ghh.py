bbf8f865c97820464a1bfb9a203c193d5e33c79e
r88w13 <r88w13@mail.ru>
2022-09-25 00:12:46 +0300

    Homework lesson 10

---
+++ ghh.py
@@ -0,0 +1,55 @@
+n = 29
+if n ==1:
+    print("Мне",n, "год")
+if n>=2 and n<=4:
+    print("Мне",n, "года")
+elif n>=5 and n<=20:
+    print("Мне",n, "лет")
+if n == 21:
+    print("Мне", n, "год")
+if n >= 22 and n <= 24:
+    print("Мне", n, "года")
+elif n >= 25 and n <= 30:
+    print("Мне", n, "лет")
+if n == 31:
+    print("Мне", n, "год")
+if n >= 32 and n <= 34:
+    print("Мне", n, "года")
+elif n >= 35 and n <= 40:
+    print("Мне", n, "лет")
+if n == 41:
+    print("Мне", n, "год")
+if n >= 42 and n <= 44:
+    print("Мне", n, "года")
+elif n >= 45 and n <= 50:
+    print("Мне", n, "лет")
+if n == 51:
+    print("Мне", n, "год")
+if n >= 52 and n <= 54:
+    print("Мне", n, "года")
+elif n >= 55 and n <= 60:
+    print("Мне", n, "лет")
+if n == 61:
+    print("Мне", n, "год")
+if n >= 62 and n <= 64:
+    print("Мне", n, "года")
+elif n >= 65 and n <= 70:
+    print("Мне", n, "лет")
+if n == 71:
+    print("Мне", n, "год")
+if n >= 72 and n <= 74:
+    print("Мне", n, "года")
+elif n >= 75 and n <= 80:
+    print("Мне", n, "лет")
+if n == 81:
+    print("Мне", n, "год")
+if n >= 82 and n <= 84:
+    print("Мне", n, "года")
+elif n >= 85 and n <= 90:
+    print("Мне", n, "лет")
+if n == 91:
+    print("Мне", n, "год")
+if n >= 92 and n <= 94:
+    print("Мне", n, "года")
+elif n >= 95 and n <= 99:
+    print("Мне", n, "лет")
---
+++ t2.py
@@ -0,0 +1,7 @@
+a = 6
+b = 7
+c = 20
+if a+b>c and b+c>a and a+c>b:
+    print ("Такой тругольник существует")
+if a+b<=c or b+c<=a or a+c<=b:
+    print("Нет такого тругольника")
